ff919388076beaa27b2407c11b61c61d34f843b3
Arjun singh <arjun9708916@gmail.com>
2026-06-18 22:40:34 +0530

    Add files via upload

---
+++ WEEK 3/DAY 4/dailytemperature.java
@@ -0,0 +1,19 @@
+class dailytemperature {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int[] ans = new int[temperatures.length];
+
+        for(int i = 0; i < temperatures.length; i++) {
+
+            for(int j = i + 1; j < temperatures.length; j++) {
+
+                if(temperatures[j] > temperatures[i]) {
+                    ans[i] = j - i;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
