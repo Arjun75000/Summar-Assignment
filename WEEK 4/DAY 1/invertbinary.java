c8e127b55a3fb776ff55d3ca11bdaf009bb6f1c6
Arjun singh <arjun9708916@gmail.com>
2026-06-22 22:09:46 +0530

    Add files via upload

---
+++ WEEK 4/DAY 1/invertbinary.java
@@ -0,0 +1,11 @@
+class invertbinary {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+
+        return root;
+    }
+}
