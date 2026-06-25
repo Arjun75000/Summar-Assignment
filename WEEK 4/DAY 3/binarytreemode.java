22c4282a27f924937b8a0353ab099e146a0c5690
Arjun singh <arjun9708916@gmail.com>
2026-06-25 22:13:07 +0530

    Add files via upload

---
+++ WEEK 4/DAY 3/binarytreemode.java
@@ -0,0 +1,24 @@
+class binarytreemode {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        inorder(root, ans);
+
+        return ans;
+    }
+
+    public void inorder(TreeNode root, List<Integer> ans) {
+
+        if(root == null) {
+            return;
+        }
+
+        inorder(root.left, ans);
+
+        ans.add(root.val);
+
+        inorder(root.right, ans);
+    }
+}
