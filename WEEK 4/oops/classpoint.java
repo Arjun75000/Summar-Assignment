e4e2d875d475b01db7db313d4bec8f7931de3237
Arjun singh <arjun9708916@gmail.com>
2026-06-27 23:10:16 +0530

    Add files via upload

---
+++ WEEK 4/oops/classpoint.java
@@ -0,0 +1,40 @@
+
+class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void display() {
+        System.out.println("x = " + x + ", y = " + y);
+    }
+
+    public static void main(String[] args) {
+        Point p = new Point(10, 20);
+        p.display();
+
+        p.setXY(30, 40);
+        p.display();
+    }
+}
